524d07a813b27c7359ccb39219723c9f8642bf6a
kyclnx <1501439779@qq.com>
2024-08-29 18:32:17 +0800

    员工信息分页查询

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
